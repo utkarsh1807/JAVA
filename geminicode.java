3ca07a94627723923d91f4321bf26c389e809a1f
Utkarsh Srivastava <66549284+utkarsh1807@users.noreply.github.com>
2021-10-02 15:21:27 +0530

    Add files via upload

---
+++ geminicode.java
@@ -0,0 +1,19 @@
+import java.util.Scanner.*;
+import java.io.*;
+
+class seperate{
+	 void fun(int A[], int len){
+		// Logic
+	}
+}
+public class geminicode{
+	public static void main(String args[]){
+		int A[] = {1,0,1,1,1,1,1,0,1,0,0,0,0,1,0,1,0};
+		int len = A.length;
+		seperate s = new seperate();
+		s.fun(A,len);
+		for (int i=0;i<len;i++){
+			System.out.println(A[i]);
+		}
+	}
+}
